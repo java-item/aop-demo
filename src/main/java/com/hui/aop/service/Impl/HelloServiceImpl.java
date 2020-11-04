49dc346b787de06361a5df0260c48b0b94cc06d9
liuqinghui <liuqinghui1991@163.com>
2020-11-04 18:47:48 +0800

    update

--- src/main/java/com/hui/aop/service/Impl/HelloServiceImpl.java
+++ src/main/java/com/hui/aop/service/Impl/HelloServiceImpl.java
@@ -1,7 +1,9 @@
 package com.hui.aop.service.Impl;
 
 import com.hui.aop.service.HelloService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String str) {
--- src/test/java/com/hui/aop/AopApplicationTests.java
+++ src/test/java/com/hui/aop/AopApplicationTests.java
@@ -1,18 +1,20 @@
 package com.hui.aop;
 
-import com.hui.aop.interceptor.Impl.MyInterceptorIml;
-import com.hui.aop.proxy.ProxyBean;
 import com.hui.aop.service.HelloService;
-import com.hui.aop.service.Impl.HelloServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class AopApplicationTests {
+    @Resource
+    private HelloService helloService;
     @Test
     void contextLoads() {
-        HelloService proxyBean = (HelloService) ProxyBean.getProxyBean(new HelloServiceImpl(), new MyInterceptorIml());
-        System.out.println(proxyBean.hello("Java"));
+        String java = helloService.hello("Java");
+        System.out.println("result -->" + java);
     }
 
 }
